9c228c2c3420d375e976c0cf5061aa390dc97e7e
Emanuel Tito <247869019+EmanuelTitoDev@users.noreply.github.com>
2026-02-20 12:39:59 -0500

    feat: add entity-relationship diagram to docs

---
+++ src/main/java/com/emanueltito/supermarket_sales_management_api/SupermarketSalesManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.emanueltito.supermarket_sales_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketSalesManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketSalesManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emanueltito/supermarket_sales_management_api/SupermarketSalesManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanueltito.supermarket_sales_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketSalesManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
